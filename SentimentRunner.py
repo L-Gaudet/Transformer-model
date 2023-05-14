acec11c7f3d1e26c71fc6c8de8e895dd06c6196d
Tyler L <69596889+tylew@users.noreply.github.com>
2023-05-14 13:04:48 -0700

    Update SentimentRunner.py

--- SentimentRunner.py
+++ SentimentRunner.py
@@ -1,38 +1,48 @@
 import torch
+from argparse import ArgumentParser
 from transformers import AutoTokenizer
 from components.TransformerClassifier import TransformerClassifier
 
-# Load the saved model
-PATH_TO_MODEL = 'TrainedClassifier'
-model = torch.load(PATH_TO_MODEL, map_location=torch.device('cpu'))
-model.eval()
+def start(arg): 
+    # Load the saved model
+    PATH_TO_MODEL = arg.filename
+    model = torch.load(PATH_TO_MODEL, map_location=torch.device('cpu'))
+    model.eval()
 
-# Set up the tokenizer
-tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased')
+    # Set up the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased')
 
-while(True):
-    # Input text
-    text = input('Please type an input')
-    # text = "This movie is poor and shit and bad!"
+    while(True):
+        # Input text
+        text = input('Please type an input')
+        # text = "This movie is poor and shit and bad!"
 
-    # Tokenize the input text
-    inputs = tokenizer.encode_plus(
-        text,
-        add_special_tokens=True,
-        max_length=512,
-        padding='max_length',
-        return_attention_mask=True,
-        return_tensors='pt'
-    )
+        # Tokenize the input text
+        inputs = tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=512,
+            padding='max_length',
+            return_attention_mask=True,
+            return_tensors='pt'
+        )
 
-    # Extract the input IDs and attention mask
-    input_ids = inputs['input_ids']
-    attention_mask = inputs['attention_mask']
+        # Extract the input IDs and attention mask
+        input_ids = inputs['input_ids']
+        attention_mask = inputs['attention_mask']
 
-    # Run the model on the input text
-    with torch.no_grad():
-        outputs = model(input_ids)
+        # Run the model on the input text
+        with torch.no_grad():
+            outputs = model(input_ids)
 
-    # Get the predicted sentiment
-    prediction = torch.argmax(outputs, dim=1)
-    print(f"Predicted sentiment: {prediction.item()}")
+        # Get the predicted sentiment
+        prediction = torch.argmax(outputs, dim=1)
+        print(f"Predicted sentiment: {prediction.item()}")
+
+if __name__ == '__main__':
+	parser = ArgumentParser()
+
+	parser.add_argument("-f", "--filename",
+						dest="filename",
+						help="Model filename",
+						default='TrainedClassifier', type=str)
